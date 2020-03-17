9ac9b7675103d4705c68fc8a8d6b6c6821726634
GeorgeLuUOL <47080847+GeorgeLuUOL@users.noreply.github.com>
2020-03-17 21:57:28 +0000

    17/3

---
+++ Assets/script/circular.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circular : MonoBehaviour
+{
+    public float speed = 1f;
+    public float force =1f;
+
+    private static float midX = 4f;
+
+    private static float midY = 0f;
+    
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity=new Vector2(speed,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currX = this.transform.position.x-midX;
+        float currY = this.transform.position.y-midY;
+        
+        float hypoteinousa = Convert.ToSingle(Math.Sqrt(currX * currX + currY * currY));
+        
+        float forceX = currX / hypoteinousa * force;
+        float forceY = currY / hypoteinousa * force;
+        rigidBody.AddForce(new Vector2(-forceX,-forceY));
+        Debug.Log(hypoteinousa);
+    }
+}
